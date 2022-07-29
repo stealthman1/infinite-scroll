c690648a527a0a29e1fe1d27099463289cbcbf39
stealthman1 <timur.marufiy@gmail.com>
2022-07-29 16:57:30 +0300

    added funcs to set attributes on DOM elements

--- script.js
+++ script.js
@@ -9,9 +9,35 @@ const apiKey = 'fUpGe9cxB1eBXCTNGcSY7VYFIctiwiimQKW33ffAWII';
 
 const apiURL = `https://api.unsplash.com/photos/random/?client_id=${apiKey}&count=${count}`;
 
+// Helper Function to Set Attributes on DOM Elements
+function setAttributes(element, attributes) {
+    for (const key in attributes) {
+        element.setAttribute(key, attributes[key]);
+    }
+}
+
 // Creat Elements For Links & Photos, Add to DOM
 function displayPhotos() {
     // Run function for each object in photosArray
+    photosArray.forEach((photo) => {
+        // Create <a> to link to Unsplash
+        const item = document.createElement('a');
+        setAttributes(item, {
+            href: photo.links.html,
+            target: '_blank',
+        });
+
+        // Create <img> for photo
+        const img = document.createElement('img');
+        setAttributes(img, {
+            src: photo.urls.regular,
+            alt: photo.alt_description,
+            title: photo.alt_description,
+        });
+        // Put <img> inside <a>, then put both inside imageContainer
+        item.appendChild(img);
+        imagContainer.appendChild(item);
+    })
 }
 
 // Get photos from Unsplash API
